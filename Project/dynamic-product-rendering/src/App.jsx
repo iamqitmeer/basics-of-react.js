0338d8706c008ef93cfc3f77fecdc38844d30a8c
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-08-03 12:01:24 +0500

    Learn Product Rendering

--- Project/dynamic-product-rendering/src/App.jsx
+++ Project/dynamic-product-rendering/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Cart from './Cart'
+import products from './Products';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  console.log(products);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center items-center flex-wrap'>
+        {products.map((data) => {
+          return <Cart title={data.title} description={data.description} price={data.price} category={data.category} image={data.image} />
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
