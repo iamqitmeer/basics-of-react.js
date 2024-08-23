cd56fefe92ac4489799d9337794b27c178ffc710
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-08-23 12:21:51 +0500

    learn react router dom

---
+++ react-router/src/Screens/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="w-full h-screen bg-gray-950 flex items-center justify-center">
+      <h1 className="text-5xl text-white font-black">Home</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ react-router/src/Screens/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Portfolio() {
+  return (
+    <div className="w-full h-screen bg-gray-950 flex items-center justify-center">
+      <h1 className="text-5xl text-white font-black">Portfolio</h1>
+    </div>
+  );
+}
+
+export default Portfolio;
